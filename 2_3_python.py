78b5b256c3372f8c3abc5e797179d130cf78094c
Theteyriry <theteyriry@gmail.com>
2025-06-14 16:20:45 +0300

    Add files via upload

---
+++ 2_3_python.py
@@ -0,0 +1,8 @@
+import math_operations as mo
+
+# Демонстрация работы 
+print("Сложение:", mo.add(5, 3))
+print("Вычитание:", mo.subtract(5, 3))
+print("Умножение:", mo.multiply(5, 3))
+print("Деление:", mo.divide(5, 3))
+print("Деление на ноль:", mo.divide(5, 0))
